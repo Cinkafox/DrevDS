4145dc6929d17ccc6913afe2220214f2627b11ed
Cinkafox <leofoxm40@gmail.com>
2022-11-02 12:02:56 +0300

    Fix

--- modules/demotivator.js
+++ modules/demotivator.js
@@ -10,8 +10,8 @@ const { createCanvas, loadImage } = require('canvas')
 const demotivatorImage = async (img, title,subtitle) => {
 
     const avatar = await loadImage(img.attachment)
-    const IHeight = avatar.height
-    const IWidth = avatar.width
+    const IWidth = 1280
+    const IHeight = IWidth * avatar.height / avatar.width
 
     const Height = IHeight*1.25
     const Width = IWidth*1.25
@@ -19,41 +19,36 @@ const demotivatorImage = async (img, title,subtitle) => {
     let textSize = Height / 8
     const coefUmen = 0.6
     
-    const StrLength = title.length
     const CanvasHeight = Height + textSize * 1.5
-    //console.log(StrLength / 1.5 * textSize,IWidth)
-    let CanvasWidthRaz = (((StrLength / 1.5 * textSize) > IWidth) ? (StrLength * textSize / 1.5 / 2) : 0)
 
-    const CanvasWidth = Width + CanvasWidthRaz
+    const CanvasWidth = Width 
     const canvas = createCanvas(CanvasWidth, CanvasHeight)
     const ctx = canvas.getContext('2d')
 
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, CanvasWidth, CanvasHeight);
 
-    const LineWidth = Width/500
+    const LineWidth = Width / 500
     ctx.lineWidth = LineWidth
     ctx.strokeStyle = "#ffffff"
 
-    CanvasWidthRaz /=2
     const raz = 4 + LineWidth
-    const AX = (Width - IWidth) / 2 + CanvasWidthRaz
+    const AX = (Width - IWidth) / 2
     const AY = (Height - IHeight) / 2
 
     ctx.strokeRect(AX, AY, IWidth, IHeight)
-    ctx.drawImage(avatar, AX + raz, AY + raz, IWidth - raz*2, IHeight - raz*2)
+    ctx.drawImage(avatar, AX + raz, AY + raz, IWidth - raz * 2, IHeight - raz * 2)
 
-    ctx.font = 'normal ' + textSize + 'px Times New Roman'
+    ctx.font = 'normal ' + CanvasWidth / ctx.measureText(title).width * 53 + '% Times New Roman'
     ctx.fillStyle = '#fff'
     ctx.textAlign = 'center'
-    ctx.fillText(title, Width / 2 + CanvasWidthRaz, IHeight*1.25+5)
+    ctx.fillText(title, Width / 2, IHeight * 1.25 + 5)
 
     textSize *= coefUmen
-
     ctx.font = 'normal ' + textSize + 'px Times New Roman'
     ctx.fillStyle = '#fff'
     ctx.textAlign = 'center'
-    ctx.fillText(subtitle, Width / 2 + CanvasWidthRaz, IHeight * 1.25 + 2 + textSize/coefUmen)
+    ctx.fillText(subtitle, Width / 2, IHeight * 1.25 + 2 + textSize / coefUmen)
 
     const buffer = canvas.toBuffer('image/png')
     return buffer
