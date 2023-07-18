9601814fbcfa0f12f88a4d86f1d2f948cae84ad0
Cinka <leofoxm40@gmail.com>
2023-07-18 16:26:24 +0300

    Sound update

--- plugins/Fir.js
+++ plugins/Fir.js
@@ -1,24 +1,11 @@
-const { MessageFlags } = require('discord.js-selfbot-v13')
-const { playMuzon } = require('../lib/MusicPlayer')
 const PluginManager = require('../lib/PluginManager')
-const fs = require('fs')
+const playMuzon = require('../lib/MusicPlayer')
+const Logger = require('../lib/Logger')
 
 PluginManager.CreatePlugin("пофырчи",(args,m) =>{
-    m.channel.sendTyping()
-    m.channel.send({
-        files: [
-            {
-                attachment: fs.readFileSync("./assets/music/fir.ogg"),
-                name: "voice-message.ogg",
-                contentType : "audio/ogg",
-                duration: 4.19,
-                waveform: 'AAANCAsHDRIJBwoLB2a0tbWgnk95Rz0='
-            }
-        ]
-        ,
-        flags : MessageFlags.FLAGS.IS_VOICE_MESSAGE
-    })
-
-    if(m.member.voice.channel != undefined)
-        playMuzon(m.member.voice.channel,"./assets/music/fir.ogg")
+    let pitch = Number(args[1])
+    if(Number.isNaN(pitch) || pitch > 3 || pitch < 0.5) pitch = 1
+    pitch = Math.round(pitch * 10) / 10
+    Logger.debug(pitch)
+    playMuzon.play(m,"./assets/music/fir.wav",pitch)
 })
--- plugins/Music.js
+++ plugins/Music.js
@@ -1,14 +1,15 @@
 const PluginManager = require('../lib/PluginManager')
 const playMuzon = require('../lib/MusicPlayer')
 
+
 PluginManager.CreatePlugin("негры",(args,m)=>{
-    playMuzon.playMuzon(m.member.voice.channel,"./assets/music/blackman.ogg")
+    playMuzon.play(m,"./assets/music/blackman.wav")
 })
 
 PluginManager.CreatePlugin("я русский",(args,m)=>{
-    playMuzon.playMuzon(m.member.voice.channel,"./assets/music/yarus.ogg")
+    playMuzon.play(m,"./assets/music/yarus.wav")
 })
 
 PluginManager.CreatePlugin("еврей",(args,m)=>{
-    playMuzon.playMuzon(m.member.voice.channel,"./assets/music/jew.ogg")
+    playMuzon.play(m,"./assets/music/jew.wav")
 })
